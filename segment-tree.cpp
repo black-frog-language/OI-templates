b5f1429128632ed41c9ca420cfe95cc0be862927
t532 <2037246484@qq.com>
2019-01-30 15:43:18 +0800

    rename: segment_tree => segment_tree_1, add segment_tree_2 with add_range & sub_range

--- segment-tree.cpp
+++ segment-tree.cpp
@@ -9,17 +9,20 @@ using std::max;
 using std::shared_ptr;
 using std::make_shared;
 
+// get: get_value; get_range_sum, get_range_min, get_range_max;
+// set: set_value, add_value, sub_value, and_value, or_value, not_value; ;
+// build: segment_tree, operator=, build
 template <typename T>
-class segment_tree {
+class segment_tree_1 {
     
 private:
     template <typename U>
-    struct segment_tree_node {
+    struct node {
         size_t from, to;
         U sum, max, min;
     };
     
-    shared_ptr<vector<segment_tree_node<T>>> tree = make_shared<vector<segment_tree_node<T>>>();
+    shared_ptr<vector<node<T>>> tree = make_shared<vector<node<T>>>();
     
     T assign(size_t pos, T val) {
         (*tree)[pos].sum = val;
@@ -123,4 +126,132 @@ public:
     }
     T get_range_min(size_t from, size_t to) const { return get_range_min(from, to, 1); }
     
+};
+
+// get: get_value; get_range_sum;
+// set: set_value, add_value, sub_value, and_value, or_value, not_value; add_range, sub_range;
+// build: segment_tree, operator=, build;
+template <typename T>
+class segment_tree_2 {
+    
+private:
+    template <typename U>
+    struct node {
+        size_t from, to;
+        U sum, lazy;
+    };
+    
+    shared_ptr<vector<node<T>>> tree = make_shared<vector<node<T>>>();
+    
+    T assign(size_t pos, T val) {
+        (*tree)[pos].sum = val;
+        return val;
+    }
+    void push_up(size_t pos) {
+        (*tree)[pos].sum = (*tree)[pos << 1].sum + (*tree)[pos << 1 | 1].sum;
+    }
+    void push_down(size_t pos) {
+        if ((*tree)[pos].lazy) {
+            (*tree)[pos << 1].lazy += (*tree)[pos].lazy;
+            (*tree)[pos << 1].sum += ((*tree)[pos << 1].to - (*tree)[pos << 1].from + 1) * (*tree)[pos].lazy;
+            (*tree)[pos << 1 | 1].lazy += (*tree)[pos].lazy;
+            (*tree)[pos << 1 | 1].sum += ((*tree)[pos << 1 | 1].to - (*tree)[pos << 1 | 1].from + 1) * (*tree)[pos].lazy;
+            (*tree)[pos].lazy = 0;
+        }
+    }
+    
+public:
+    segment_tree() {}
+    segment_tree(segment_tree<T> &init) {
+        build(init);
+    }
+    segment_tree(vector<T> &init) {
+        build(init);
+    }
+    segment_tree(size_t size) {
+        build(size);
+    }
+    segment_tree(size_t size, T val) {
+        build(size, val);
+    }
+    segment_tree(initializer_list<T> &init) {
+        build(*make_shared<vector<T>>(init));
+    }
+    
+    void build(segment_tree<T> &init) { tree = make_shared<vector<T>>(init.tree); }
+    void build(vector<T> &init, size_t from, size_t to, size_t root) {
+        tree->resize(init.size() * 4);
+        (*tree)[root].from = from;
+        (*tree)[root].to = to;
+        if (from == to) assign(root, init[from]);
+        else {
+            size_t mid = (from + to) >> 1;
+            build(init, from, mid, root << 1);
+            build(init, mid + 1, to, root << 1 | 1);
+            push_up(root);
+        }
+    }
+    void build(vector<T> &init) { build(init, 0, init.size() - 1, 1); }
+    void build(size_t size, T val) { build(*make_shared<vector<T>>(size, val)); }
+    void build(size_t size) { build(size, 0); }
+    
+    void operator=(segment_tree<T> &init) { build(init); }
+    void operator=(vector<T> &init) { build(init); }
+    
+    T get_value(size_t pos, size_t root = 1) {
+        if ((*tree)[root].from == (*tree)[root].to) return (*tree)[root].sum;
+        push_down(root);
+        size_t mid = ((*tree)[root].from + (*tree)[root].to) >> 1;
+        if (pos <= mid) return get_value(pos, root << 1);
+        else return get_value(pos, root << 1 | 1);
+    }
+    
+    T set_value(size_t pos, T (&transform)(T original, T val) = [] (T original, T val) { return val; }, size_t root = 1) {
+        if ((*tree)[root].from == (*tree)[root].to) {
+            int val = transform((*tree)[root].sum);
+            assign(root, val);
+            return val;
+        }
+        push_down(root);
+        size_t mid = ((*tree)[root].from + (*tree)[root].to) >> 1;
+        T result;
+        if (pos <= mid) result = set_value(pos, transform, root << 1);
+        else result = set_value(pos, transform, root << 1 | 1);
+        push_up(root);
+        return result;
+    }
+    
+    T add_value(size_t pos, T val = 1) { return set_value(pos, val, [] (T original, T val) { return original + val; }); }
+    T sub_value(size_t pos, T val = 1) { return add_value(pos, -val); }
+    
+    T and_value(size_t pos, T val) { return set_value(pos, val, [val] (T original) { return original & val; }); }
+    T or_value(size_t pos, T val) { return set_value(pos, val, [val] (T original) { return original | val; }); }
+    T not_value(size_t pos) { return set_value(pos, [] (T original) { return ~original; }); }
+    
+    T get_range_sum(size_t from, size_t to, size_t root = 1) {
+        if (from == (*tree)[root].from && to == (*tree)[root].to) return (*tree)[root].sum;
+        push_down(root);
+        size_t mid = ((*tree)[root].from + (*tree)[root].to) >> 1;
+        if (to <= mid) return get_range_sum(from, to, root << 1);
+        else if (from > mid) return get_range_sum(from, to, root << 1 | 1);
+        else return get_range_sum(from, mid, root << 1) + get_range_sum(mid + 1, to, root << 1 | 1);
+    }
+    
+    void add_range(size_t from, size_t to, T val, size_t root = 1) {
+        if (from == (*tree)[root].from && to == (*tree)[root].to) {
+            (*tree)[root].sum += ((*tree)[root].to - (*tree)[root].from + 1) * val;
+            (*tree)[root].lazy += val;
+            return;
+        }
+        push_down(root);
+        size_t mid = ((*tree)[root].from + (*tree)[root].to) / 2;
+        if (to <= mid) add_range(from, to, val, root << 1);
+        else if (from > mid) add_range(from, to, val, root << 1 | 1);
+        else {
+            add_range(from, mid, val, root << 1);
+            add_range(mid + 1, to, val, root << 1 | 1);
+        }
+        push_up(root);
+    }
+    void sub_range(size_t from, size_t to, T val, size_t root = 1) { return add_range(from, to, -val, root); }
 };
